4fc8559827cb365e2539553e69c1621d3b6b4941
Hunter Hayward <hunter@webauthn.works>
2020-01-17 13:52:36 -0700

    lab0

---
+++ lab0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Hunter Hayward
+* Lab 0, CSCI 112
+* 1/17/2020
+*/
+
+int main(void){
+    //prints"heelo World"
+    printf("Hello World\n");
+    //exit with no errors
+    return(0);
+}
