f15aafe8e3aca8cd5bdca2e634f0420138f23bed
hunter.hayward <m25m754@csci112.cs.montana.edu>
2020-04-29 12:59:39 -0600

    Finished with Test3 programs

--- Final_Programs/it3pgm2.c
+++ Final_Programs/it3pgm2.c
@@ -4,7 +4,32 @@
 
 // add Linked List
 
+typedef struct node_t {
+	int NUM;
+	struct node_t * next;
+} Node_t;
+
+// Inserts the numbers as they are read from the file
+
+void ListInsert(Node_t** head, int num){
+	Node_t* new_node = malloc(sizeof(Node_t));
+	new_node->NUM = num;
+	new_node->next = NULL;
+
+	Node_t* old_head = *head;
+	*head = new_node;
+	
+	new_node->next = old_head;
+
+	return;
+
+
+}
+
+
 int main(void) {
+
+	Node_t* list_head = NULL;
 
 	FILE *file_in = fopen("numbers.txt", "r");
 
@@ -16,10 +41,17 @@ int main(void) {
 
 	int bufferLength = 60;
 	char buffer[bufferLength];
-
+	int num;
 	while(fgets(buffer, bufferLength, file_in)) {
-		printf("%s\n", buffer);
+		num = atoi(buffer);
+		ListInsert(&list_head, num);
 	}
+	// prints the list from the head 
+	Node_t* cur = list_head;
+	while(cur != NULL) {
+		printf("Numbers: %d\n", cur->NUM);
 
+		cur = cur->next;
+	}
 	
 }
--- Final_Programs/it3pgm3.c
+++ Final_Programs/it3pgm3.c
@@ -6,7 +6,7 @@
 int main(void) {
 	
 	FILE *file_in = fopen("doubles.txt", "r");
-	
+	FILE *file_out = fopen("Out_file.bin", "wb");	
 	if (file_in == NULL) {
 		printf("Error!, File does not exist");
 		exit(1);
@@ -14,10 +14,22 @@ int main(void) {
 
 	int bufferLength = 60;
 	char buffer[bufferLength];
-
+	
+	double Array[20];
+	int i = 0;
 	while(fgets(buffer, bufferLength, file_in)) {
-		printf("%s\n", buffer);
-	}
+		double num = strtod(buffer, NULL);
 
+		Array[i] = num;
+		++i;	
+	}
+	
+	fwrite(Array, 1, sizeof(Array), file_out);
+	
+	double sum;
+	for (int l = 0; l < i; l++) {
+		sum += Array[l];
+	}
+	printf("Sum = %lf\n",sum);
 
 }
